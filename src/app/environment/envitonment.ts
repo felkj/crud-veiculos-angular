05ed7b447fd341c76454cbc571c830305c17c853
Felipe Silva <96023311+felkj@users.noreply.github.com>
2025-06-12 08:41:49 -0300

    Update envitonment.ts

--- src/app/environment/envitonment.ts
+++ src/app/environment/envitonment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'crud-veiculos-java-production.up.railway.app',
+    apiUrl: 'https://crud-veiculos-java-production.up.railway.app',
 };
